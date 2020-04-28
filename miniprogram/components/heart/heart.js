b0c835e5ab80e949e504d9b64ac7f7f3125301b3
hugo <hundren@qq.com>
2020-04-28 17:36:38 +0800

    fix: 优化浮动小心心

--- miniprogram/components/heart/heart.js
+++ miniprogram/components/heart/heart.js
@@ -17,12 +17,12 @@ Component({
       // 增加动画特效
         const type = ['1','2','3','4']
         let hearts = []
-        for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 3; index++) {
           hearts.push({
             'y':type[Math.floor(Math.random()*type.length)],
             'x':type[Math.floor(Math.random()*type.length)],
             's':type[Math.floor(Math.random()*type.length)],
-            'delay':index*0.8
+            'delay':index*1.2
           })
         }
         this.setData({
