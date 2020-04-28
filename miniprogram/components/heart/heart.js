adf5b44d634c75d37d5462b006db86f961225f80
hugo <hundren@qq.com>
2020-04-28 17:01:50 +0800

    feat: 增加浮动的心心

--- miniprogram/components/heart/heart.js
+++ miniprogram/components/heart/heart.js
@@ -9,10 +9,31 @@ Component({
     }
   },
   data: {
-  
+    hearts:[]
   }, // 私有数据，可用于模版渲染
 
-
+  lifetimes: {
+    attached: function() {
+      // 增加动画特效
+        const type = ['1','2','3','4']
+        let hearts = []
+        for (let index = 0; index < 7; index++) {
+          hearts.push({
+            'y':type[Math.floor(Math.random()*type.length)],
+            'x':type[Math.floor(Math.random()*type.length)],
+            's':type[Math.floor(Math.random()*type.length)],
+            'delay':index*0.7
+          })
+        }
+        this.setData({
+          hearts
+        })
+     
+    },
+    detached: function() {
+      // 在组件实例被从页面节点树移除时执行
+    },
+  },
   methods: {
     sign:function(){
       wx.requestSubscribeMessage({
